654b3a04b93d0e4f7f76433fd268ca89750162fb
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-26 20:38:12 +0200

    Creating <> display method  to vehicals class

--- car-auto.py
+++ car-auto.py
@@ -2,9 +2,11 @@ class Vehicals :
     def __init__ (self ,brand,model,year,rental):
         self.brand=brand 
         self.model=model
-        self.year=year
-        self.rental_price_per_day=rental
+        self.year=int(year)
+        self.rental_price_per_day=int(rental)
+    def display_info(self): 
+        print(f"the {self.brand} vehicals model is {self.model} and the module year is {self.year} and the rental price is {self.rental_price_per_day}$")
 
 
-
-        
+v= Vehicals("Audi","r9",2026,150)
+v.display_info()
