4d260f468efb54426bac916564f2c6f7ed688940
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-26 21:31:07 +0200

    making <>  a list of class Vehicals and asking the user to enter the cars details and save it in a list

--- car-auto.py
+++ car-auto.py
@@ -7,6 +7,19 @@ class Vehicals :
     def display_info(self): 
         print(f"the {self.brand} vehicals model is {self.model} and the module year is {self.year} and the rental price is {self.rental_price_per_day}$")
 
-
+vehicals=[Vehicals]   
 v= Vehicals("Audi","r9",2026,150)
 v.display_info()
+stop=1
+stop = int(input("add a car/ if no press 0 : "))
+while stop !=0:
+     car_brand = input("Brand :")
+     car_model = input("model :")
+     car_year  = int(input("year :"))
+     car_rental_price = int(input("Price :"))
+     car= Vehicals(car_brand,car_model,car_year,car_rental_price)
+     vehicals.append(car)
+     stop = int(input("add another car/ if no press 0 : "))
+for vehical in vehicals:
+    vehical.display_info()
+    
