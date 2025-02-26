8ac0e151ba8409b1aafe16ecdb7cf60a4fbe343c
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-26 20:27:15 +0200

    Creating <> attruibutes to vehicals class

---
+++ car-auto.py
@@ -0,0 +1,10 @@
+class Vehicals :
+    def __init__ (self ,brand,model,year,rental):
+        self.brand=brand 
+        self.model=model
+        self.year=year
+        self.rental_price_per_day=rental
+
+
+
+        
