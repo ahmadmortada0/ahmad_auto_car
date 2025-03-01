836313c5995afc43422c1e09572f8805a7871d99
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-03-01 19:30:55 +0200

    Creating <> Bike class and doind some instances of each class

--- car-auto.py
+++ car-auto.py
@@ -5,24 +5,50 @@ class Vehicals :
         self.year=int(year)
         self.__rental_price_per_day=int(rental)
     def display_info(self): 
-        return f"the {self.brand} vehicals model is {self.model} and the module year is {self.year} and the rental price is {self.__rental_price_per_day}$"
+        return f" {self.brand}  {self.model}, Year  : {self.year}, Rental price : ${self.__rental_price_per_day}/day"
     def calculate_rental_cost(self,days):
-        return days*self.__rental_price_per_day
+        return f"Rental cost for {self.brand} {self.model} for {days} days is :${days*self.__rental_price_per_day}"
     def setRental(self,amount):
+        if self.__rental_price_per_day!=amount:
+          print(f"Updated rental price for {self.brand} {self.model} is : ${amount}/day")  
         self.__rental_price_per_day=amount
     def getRental(self):
         return self.__rental_price_per_day
+    def showVehicalInfo(self,Vehical):
+        self.display_info()
+        
     
 class Car(Vehicals):
     def __init__(self, brand, model, year, rental,seating_capacity):
         super().__init__(brand, model, year, rental)
         self.seating_capacity=seating_capacity
+       
+
     def display_info(self):
-        print(super().display_info()+f" and seating capacity is {self.seating_capacity}")
+        print(f"Car :{super().display_info()} and seating capacity is {self.seating_capacity}")
+
+class Bike(Vehicals):
+    def __init__(self, brand, model, year, rental,engine_capacity):
+        super().__init__(brand, model, year, rental)
+        self.engine_capacity=str(engine_capacity)+"CC"
+
+    def display_info(self):
+        print(f"Bike :{super().display_info()} and the engine capacity is {self.engine_capacity}")
 
 
-car = Car("mer","C",2020,1500,4)
+car = Car("Toyota","Corolla",2020,50,5)
+bike=Bike("Yamaha","r1",2019,50,998)
+
+bike.display_info()
 car.display_info()
+print(car.calculate_rental_cost(5))
+print(bike.calculate_rental_cost(15))
+car.setRental(55)
+print(car.calculate_rental_cost(5))
+car.showVehicalInfo(car)
+bike.showVehicalInfo(bike)
+
+
 # vehicals=[Vehicals]   
 # v= Vehicals("Audi","r9",2026,150)
 # print(v.calculate_rental_cost(20))
